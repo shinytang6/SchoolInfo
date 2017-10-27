3d45dc4ee342ece527d17dbbab851e57490e43da
shinytang6 <1074461480@qq.com>
2017-10-27 10:55:21 +0800

    change laravel version to 5.2

--- backend/app/User.php
+++ backend/app/User.php
@@ -2,13 +2,10 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Request;
+
 class User extends Authenticatable
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -26,8 +23,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function signup(){
-        return ["Hello"=>"world"];
-    }
 }
